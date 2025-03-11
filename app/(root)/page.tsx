5839129f99d3e94917409f233b16f516193817d9
unknown <guilhermedeoliveiradesouza@gmail.com>
2025-03-11 14:10:08 -0300

    Add utility functions and update Tailwind CSS configuration for improved styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Conect With Entrepeneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideias, Vote on Pitches, and Get Noticed in Virtual
+          Competitions
+        </p>
+      </section>
     </>
   );
 }

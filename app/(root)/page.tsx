1cbef12292c1a4c9a4025a15b3f94fd0eddf4f6b
unknown <guilhermedeoliveiradesouza@gmail.com>
2025-03-14 09:56:36 -0300

    Author Authentication Flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import StartupCard from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -11,6 +12,10 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session = await auth();
+
+  console.log(session?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
